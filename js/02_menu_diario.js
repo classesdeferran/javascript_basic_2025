9a614d0a352bb296feec2e36aa66c90103dedf9f
Ferran <ferran.docencia@gmail.com>
2025-07-17 10:12:06 +0200

    Variables

---
+++ js/02_menu_diario.js
@@ -0,0 +1,31 @@
+// alert("Soy un alert")
+// let respuesta1 = confirm("¿Hoy es martes?")
+// alert(respuesta1)
+
+// let respuesta2 = prompt("¿Qué día es?")
+// alert(respuesta2)
+
+// let numero_primo_1 = 19 // snakecase
+// let numeroPrimo2 = 19 // camelcase
+
+let diaSemana = prompt("¿Qué día de la semana es?")
+
+if (diaSemana == "lunes") {
+    alert("toca comer pizza");
+} else if (diaSemana == "martes") {
+    alert("toca comer ensalada");
+} else if (diaSemana == "miercoles") {
+    alert("toca comer fricandó");
+} else if (diaSemana == "jueves") {
+    alert("toca comer paella");
+} else if (diaSemana == "viernes") {
+    alert("toca comer hamburguesa");
+}  else if (diaSemana == "sabado") {
+    alert("toca comer salmorejo");
+} else if (diaSemana == "domingo") {
+    alert("toca comer lentejas");
+} else {
+    alert("día de la semana incorrecto");
+}
+
+
